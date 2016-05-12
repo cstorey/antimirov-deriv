da2687e72ab9f56974e6dca4cdb9662d71e972e9
Ceri Storey <ceri@lshift.net>
2016-05-12 15:42:52 +0100

    Recursive NFA building/matching.

--- src/lib.rs
+++ src/lib.rs
@@ -1,12 +1,14 @@
 // http://semantic-domain.blogspot.co.uk/2013/11/antimirov-derivatives-for-regular.html
 
+#[macro_use]
+extern crate maplit;
 use std::collections::{BTreeMap,BTreeSet, VecDeque};
 use std::rc::Rc;
 use std::ops::BitOr;
 
 #[derive(Clone, Debug, PartialEq, Eq, Hash, PartialOrd, Ord)]
-enum Re {
-    Byte(u8),
+pub enum Re {
+    Byte(char),
     Nil,
     Bot,
     Seq (RcRe, RcRe),
@@ -20,7 +22,7 @@ let rec null = function
   | Nil | Star _ -> true
   | Alt(r1, r2) -> null r1 || null r2
   | Seq(r1, r2) -> null r1 && null r2
-   
+
    */
 
 /*
@@ -32,9 +34,9 @@ let rec null = function
 */
 
 #[derive(Clone, Debug, PartialEq, Eq, Hash, PartialOrd, Ord)]
-struct RcRe(Rc<Re>);
+pub struct RcRe(Rc<Re>);
 #[derive(Clone, Debug, PartialEq, Eq, Hash, PartialOrd, Ord)]
-struct Res(BTreeSet<RcRe>);
+pub struct Res(BTreeSet<RcRe>);
 // type M<Y> = BTreeMap<R, Y>;
 
 impl Res {
@@ -46,7 +48,7 @@ impl Res {
         Res(self.0.iter().map(f).collect())
     }
 
-    fn deriv(&self, c: u8) -> Res {
+    fn deriv(&self, c: char) -> Res {
         Res(self.0.iter().flat_map(|r| r.deriv(c).0.into_iter()).collect())
     }
 
@@ -80,49 +82,184 @@ impl RcRe {
     fn nil() -> RcRe {
         RcRe(Rc::new(Re::Nil))
     }
-    fn lit(c: u8) -> RcRe {
+    fn lit(c: char) -> RcRe {
         RcRe(Rc::new(Re::Byte(c)))
     }
     fn seq(l: RcRe, r: RcRe) -> RcRe {
         RcRe(Rc::new(Re::Seq(l, r)))
     }
+    fn alt(l: RcRe, r: RcRe) -> RcRe {
+        RcRe(Rc::new(Re::Alt(l, r)))
+    }
     fn star(r: RcRe) -> RcRe {
         RcRe(Rc::new(Re::Star(r)))
     }
-    
-    /*
-let rec deriv c = function
-  | C c' when c = c' -> R.singleton Nil 
-  | C _ | Nil | Bot -> R.empty
-  | Alt(r, r') -> R.union (deriv c r) (deriv c r')
-  | Seq(r1, r2) -> R.union (rmap (fun r1' -> Seq(r1', r2)) (deriv c r1))
-                           (if null r1 then deriv c r2 else R.empty)
-  | Star r -> rmap (fun r' -> Seq(r', Star r)) (deriv c r)
-       */
-    fn deriv(&self, c:u8) -> Res {
+
+    pub fn deriv(&self, c:char) -> Res {
         match &*self.0 {
             &Re::Byte(m) if c == m => Res(vec![RcRe::nil()].into_iter().collect()),
             &Re::Byte(_) | &Re::Nil | &Re::Bot => Res::new(),
             &Re::Alt(ref l, ref r) => &l.deriv(c) | &r.deriv(c),
-            &Re::Seq(ref l, ref r) => 
+            &Re::Seq(ref l, ref r) =>
                 &l.deriv(c).rmap(|l2| RcRe::seq(l2.clone(), r.clone()))
                 | &if l.is_null() { r.deriv(c) } else { Res::new() },
             &Re::Star(ref r) => r.deriv(c).rmap(|r2| RcRe::seq(r2.clone(), RcRe::star(r.clone()))),
         }
     }
+
+    /// From Antimirov:
+    ///
+    ///
+    pub fn lf(&self) -> BTreeSet<(char, RcRe)> {
+        match &*self.0 {
+            &Re::Nil | &Re::Bot => btreeset!{},
+            &Re::Byte(m) => btreeset!{(m, RcRe::nil())},
+            &Re::Alt(ref l, ref r) => &l.lf() | &r.lf(),
+            &Re::Star(ref r) => Self::prod(r.lf(), self.clone()),
+            &Re::Seq(ref l, ref r) => Self::prod(l.lf(), r.clone())
+                .union(&if l.is_null() { btreeset!{} } else { r.lf() })
+                .cloned()
+                .collect()
+        }
+    }
+    fn prod(l: BTreeSet<(char, RcRe)>, t: RcRe) -> BTreeSet<(char, RcRe)> {
+        match &*t.0 {
+            &Re::Nil => btreeset!{},
+            &Re::Bot => l,
+            _ => l.into_iter()
+                    .map(|(x, ref p)| (x, if p == &Self::nil() { t.clone() } else { Self::seq(p.clone(), t.clone()) })).collect()
+        }
+    }
+
+
+    /// From Antimirov:
+    ///
+    /// (PD[0], Δ[0], τ[0]) = (∅, {t}, ∅)
+    ///
+    /// PD[i+1] := PD[i] ∪  Δ[i]
+    ///
+    /// Δ[i+1] := ⋃(p∈ Δ[i]) {q | (x, q) ∈ lf(p)∧q ∉ PD[i+1]}
+    /// τ[i+1] := τ[i]∪  {(p, x, q) | p ∈ Δ[i] ∧ (x, q) ∊ lf(p)}
+    ///
+    pub fn make_nfa(&self) -> NFA {
+        #[derive(Debug, Default, PartialEq, Eq, Hash)]
+        struct State {
+            pd: BTreeSet<RcRe>,
+            delta: BTreeSet<RcRe>,
+            tau: BTreeSet<(RcRe, char, RcRe)>,
+        }
+        let mut state : State = Default::default();
+        state.delta.insert(self.clone());
+
+        for _ in 0..15 {
+            let mut next : State = Default::default();
+            // println!("S: {:?}", state);
+            next.pd.extend(state.pd.iter().cloned());
+            next.pd.extend(state.delta.iter().cloned());
+            next.delta = state.delta.iter()
+                .flat_map(|p| p.lf().into_iter().map(|(x, q)| q))
+                .filter(|q| !next.pd.contains(q))
+                .collect();
+
+            next.tau = state.tau.iter().cloned().chain(
+                    state.delta.iter()
+                    .flat_map(|p| p.lf().into_iter().map(move |(x, q)| (p.clone(), x, q))))
+                    .collect();
+
+            println!("N@{:16x}: pd: {:?}; delta: {:?}; tau: {:?}",
+                    hash(&next), next.pd.len(), next.delta.len(), next.tau.len());
+
+            // if state == next { break; }
+            state = next;
+        }
+
+        let idx = state.pd.iter().enumerate().map(|(i, r)| (r.clone(), i)).collect::<BTreeMap<_, _>>();
+
+        let mut transitions = btreemap!{};
+        for (p, x, q) in state.tau {
+            println!("{:?}; {:?}; {:?}", idx[&p], x, idx[&q]);
+            let pi = idx[&p]; let qi = idx[&q];
+            transitions.entry((pi, x)).or_insert_with(|| btreeset!{}).insert(qi);
+        }
+
+        let initial = idx[self];
+        let finals = state.pd.iter().filter(|p| p.is_null()).map(|p| idx[p]).collect();
+
+        NFA { transition: transitions, initial: initial, finals: finals }
+    }
 }
 
+fn hash<T : ::std::hash::Hash>(x: T) -> u64 {
+    use std::hash::*;
+    let mut h = SipHasher::new();
+    x.hash(&mut h);
+    h.finish()
+}
+
+impl std::ops::Mul for RcRe {
+    type Output = Self;
+    fn mul(self, other: Self) -> Self {
+        RcRe::seq(self, other)
+    }
+}
+
+impl std::ops::Add for RcRe {
+    type Output = Self;
+    fn add(self, other: Self) -> Self {
+        RcRe::alt(self, other)
+    }
+}
+
+
+#[derive(Clone, Debug, PartialEq, Eq, Hash, PartialOrd, Ord, Default)]
+pub struct NFA {
+    initial: usize,
+    transition: BTreeMap<(usize, char), BTreeSet<usize>>,
+    finals: BTreeSet<usize>,
+}
+
+impl NFA {
+    fn matches(&self, s: &str) -> bool {
+        fn try_match(nfa: &NFA, mut state: usize, mut iter: std::str::Chars, lvl: usize) -> bool {
+            while let Some(c) = iter.next() {
+                let t = nfa.transition.get(&(state, c));
+                print!("{0:1$}", "", lvl);
+                println!("state: {:?}; char: {:?}; t: {:?}", state, c, t);
+                for next in t.into_iter().flat_map(|x| x) {
+                    print!("{0:1$}", "", lvl);
+                    println!("try: {:?}, {:?} -> {:?}", state, c, next);
+                    let matchp = try_match(nfa, *next, iter.clone(), lvl+2);
+                    print!("{0:1$}", "", lvl*2);
+                    println!("matches from: {:?} -> {:?}", next, matchp);
+                    if matchp {
+                        return true
+                    }
+                    print!("{0:1$}", "", lvl); println!("try again: {:?}, {:?}", state, c);
+                }
+                print!("{0:1$}", "", lvl); println!("Out of options!");
+                return false
+            }
+            let matchp = nfa.finals.contains(&state);
+            print!("{0:1$}", "", lvl);
+            println!("END: {:?}, final? {:?}", state, matchp);
+            matchp
+        }
+        println!("Matching: {:?} against {:?}", s, self);
+        try_match(self, self.initial, s.chars(), 0)
+    }
+}
 
 #[derive(Clone, Debug, PartialEq, Eq, Hash, Default)]
 struct DFA {
     size: usize,
     fail: usize,
-    trans: Vec<(usize, u8, usize)>,
+    trans: Vec<(usize, char, usize)>,
     finalp: BTreeSet<usize>,
 }
 
-fn charfold<F: Fn(V, u8) -> V, V>(f: F, zero: V) -> V {
-    (0u8..256).fold(zero, f)
+/*
+fn charfold<F: Fn(V, char) -> V, V>(f: F, zero: V) -> V {
+    (0char..256).fold(zero, f)
 }
 
 impl DFA{
@@ -146,20 +283,39 @@ impl DFA{
         dfa
     }
 }
+*/
+
 
 #[cfg(test)]
 mod tests {
     use super::{RcRe,Res};
     #[test]
     fn it_works() {
-        let re = RcRe::star(RcRe::lit(b'a'));
+        let re = RcRe::star(RcRe::lit('a'));
         println!("{:?} -> {:?}", re, re.is_null());
         assert!(re.is_null());
-        let deriv1 = re.deriv(b'a');
+        let deriv1 = re.deriv('a');
         println!("{:?} / {:?} -> {:?}; {:?}", re, "a", deriv1, deriv1.is_null());
         assert!(deriv1.is_null());
-        let deriv2 = deriv1.deriv(b'a');
+        let deriv2 = deriv1.deriv('a');
         println!("{:?} / {:?} -> {:?}; {:?}", re, "aa", deriv2, deriv2.is_null());
         assert!(deriv2.is_null());
+    }
+
+    #[test]
+    fn should_build_nfa() {
+        use super::RcRe as R;
+        let re = R::lit('a') * R::lit('b') * R::lit('a') * R::lit('b') +
+                R::lit('a') * R::lit('b') * R::lit('b') * R::lit('b');
+        println!("Re: {:?}", re);
+        let nfa = re.make_nfa();
+        println!("NFA: {:?}", nfa);
+        println!("---");
+        assert!(nfa.matches("abbb"));
+        println!("---");
+        assert!(nfa.matches("abab"));
+        println!("---");
+        assert!(!nfa.matches("abaa"));
+        assert!(!nfa.matches("aba"));
     }
 }
