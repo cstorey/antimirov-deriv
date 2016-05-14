801bca01ca3cf1964182a3b8296ea77f5e0e565f
Ceri Storey <cez@necrofish.org.uk>
2016-05-14 17:37:15 +0100

    Ensure we bench the recursive version too.

--- benches/trivial.rs
+++ benches/trivial.rs
@@ -13,6 +13,7 @@ mod antimorov {
 
         let nfa = re.make_nfa();
         let arg = "abba";
+        b.bytes = arg.len() as u64;
         assert!(!nfa.matches(&arg));
         b.iter(|| nfa.matches(&arg))
     }
@@ -24,11 +25,39 @@ mod antimorov {
 
         let nfa = re.make_nfa();
         let arg = "abab";
+        b.bytes = arg.len() as u64;
         assert!(nfa.matches(&arg));
         b.iter(|| nfa.matches(&arg))
     }
 }
 
+mod antimorov_rec {
+    use antimirov::RcRe as R;
+    use test;
+    #[bench]
+    fn non_matching(b: &mut test::Bencher) {
+        let re = R::lit('a') * R::lit('b') * R::lit('a') * R::lit('b') +
+                 R::lit('a') * R::lit('b') * R::lit('b') * R::lit('b');
+
+        let nfa = re.make_nfa();
+        let arg = "abba";
+        b.bytes = arg.len() as u64;
+        assert!(!nfa.matches_rec(&arg));
+        b.iter(|| nfa.matches_rec(&arg))
+    }
+
+    #[bench]
+    fn matching_ok(b: &mut test::Bencher) {
+        let re = R::lit('a') * R::lit('b') * R::lit('a') * R::lit('b') +
+                 R::lit('a') * R::lit('b') * R::lit('b') * R::lit('b');
+
+        let nfa = re.make_nfa();
+        let arg = "abab";
+        b.bytes = arg.len() as u64;
+        assert!(nfa.matches_rec(&arg));
+        b.iter(|| nfa.matches_rec(&arg))
+    }
+}
 
 mod rust_regex {
     use regex::Regex;
@@ -38,6 +67,7 @@ mod rust_regex {
         let re = Regex::new(r"^(abab|abbb)$").unwrap();
 
         let arg = "abba";
+        b.bytes = arg.len() as u64;
         assert!(!re.is_match(&arg));
         b.iter(|| re.is_match(&arg))
     }
@@ -47,6 +77,7 @@ mod rust_regex {
         let re = Regex::new(r"^(abab|abbb)$").unwrap();
 
         let arg = "abab";
+        b.bytes = arg.len() as u64;
         assert!(re.is_match(&arg));
         b.iter(|| re.is_match(&arg))
     }
